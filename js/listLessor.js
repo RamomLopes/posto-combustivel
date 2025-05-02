0234873eba922890dc2d058b67ed457180886a93
RamomLopes <ramoncezar531@gmail.com>
2025-05-02 14:38:27 -0300

    feat: added main page

---
+++ js/listLessor.js
@@ -0,0 +1,18 @@
+import data from './data.js';
+
+const gridLessor = document.querySelector('.lessor-grid');
+const gridThird = document.querySelector('.third-grid');
+
+function fillGrid() {
+
+    data.forEach(element => {
+        let span = document.createElement('span');
+        span.textContent = element.name;
+
+        !element.third ? gridLessor.appendChild(span) : gridThird.appendChild(span)    
+    });
+    
+
+}
+
+fillGrid();
